4eccc3233911170291bcfe4c6d62080967045343
unknown <arora.ankit@L-G8DHG43.jasperindia.local>
2015-07-26 17:26:11 +0530

    placeholder

--- public/core.js
+++ public/core.js
@@ -17,7 +17,7 @@ function mainController($scope, $http){
 			.success(function(data){
 				$scope.formData = {}; // clear the form so our user is ready to enter another
                 $scope.todos = data;
-                console.log(data);
+                //console.log(data);
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
